a83ee0025a96e4132198c31bbf77e74b14fbfd08
NinaShved <neenas@mail.ru>
2020-09-12 00:32:43 +0300

    Создаю моего самого первого бота

---
+++ botic.py
@@ -0,0 +1,33 @@
+import logging
+from telegram.ext import Updater, CommandHandler,MessageHandler, Filters
+import settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
+
+def greet_user(update, context):
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+    print('Вызван /start')
+
+def talk_to_me(update, context):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def main():
+    # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    # Командуем боту начать ходить в Telegram за сообщениями
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    # Запускаем бота, он будет работать, пока мы его не остановим принудительно
+    mybot.idle()
+
+if __name__ == "__main__":
+    main()
+
